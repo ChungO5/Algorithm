419c802671bcf13d4fc40573fac2d981d9cd3983
BugO <103034239+ChungO5@users.noreply.github.com>
2024-09-15 15:09:24 +0900

    [level 0] Title: 커피 심부름, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,11 @@
+function solution(order) {
+    var answer = 0;
+    for (let i of order){
+        if (i.includes('cafelatte')){
+            answer += 5000
+        } else {
+            answer += 4500
+        }
+    }
+    return answer;
+}
