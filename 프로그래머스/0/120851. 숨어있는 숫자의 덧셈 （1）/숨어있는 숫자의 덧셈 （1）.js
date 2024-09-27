3f4869b77e79e70222e592c771a841af0b8cd2e0
BugO <103034239+ChungO5@users.noreply.github.com>
2024-09-27 13:57:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,8 +1,8 @@
 function solution(my_string) {
     var answer = 0;
     for (let i=0; i < my_string.length; i++){
-        if (Number(my_string[i]) in [0, 1,2,3,4,5,6,7,8,9]){
-            answer += Number(my_string[i])
+        if (Number(my_string[i]) in [0,1,2,3,4,5,6,7,8,9]){
+            answer += my_string[i]*1
         }
     }
     return answer;
