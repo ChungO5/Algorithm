5347faed84807211730717a63b761e554c5715e3
BugO <103034239+ChungO5@users.noreply.github.com>
2024-09-30 08:35:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,9 +1,8 @@
 function solution(numbers) {
     let strNum = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
-    let num = [0,1,2,3,4,5,6,7,8,9]
     let answer = numbers
     for (let i=0; i<10;i++){
-        answer = answer.replaceAll(strNum[i], num[i])
+        answer = answer.replaceAll(strNum[i], i)
     }
     return answer*1;
 }
