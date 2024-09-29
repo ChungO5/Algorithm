069b9e214419843e66af68c1d6b896e77edcf545
BugO <103034239+ChungO5@users.noreply.github.com>
2024-09-30 08:25:54 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,3 +1,8 @@
 function solution(numbers, k) {
-    return numbers[(k-1)*2 % numbers.length]
+    if (numbers.length % 2 !== 0){
+        return (k+1)*2 % numbers.length
+    } else {
+        console.log((k+1)*2,  (numbers.length / 2))
+        return numbers[k*2 / (numbers.length / 2) + 1]
+    }
 }
