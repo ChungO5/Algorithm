913db619b21e83a541a809199966630ff0fcb17e
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-02 13:50:14 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,5 +1,4 @@
 function solution(order) {  
-    let numArr = String(order).split('')
-    return numArr.filter((el) => el === '3' || el === '6' || el === '9').length
+    return [...String(order)].filter((el)=> '369'.includes(el)).length
 
 }
