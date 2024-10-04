5b6c81e56fd8476ca87f2d6758d6886166917b9d
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-04 18:22:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    if (num === 1) return 0;
+    let answer = 0;
+
+    while (true) {
+        if (answer >= 500) return -1;
+
+        if (num === 1) return answer;
+
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num = num * 3 + 1;
+        }
+        answer++;
+    }
+}
