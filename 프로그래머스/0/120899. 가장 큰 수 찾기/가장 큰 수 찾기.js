5188148c03745be0e9f06d3f7118733d4ee5f4d3
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-07 14:02:08 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,10 +1,4 @@
 function solution(array) {
-    let answer = [0, 0];
-    for (let i=0; i<array.length; i++){
-        if (array[i] > answer[0]){
-            answer[0] = array[i]
-            answer[1] = i
-        }
-    }
-    return answer;
+    let max = Math.max(...array)
+    return [max, array.indexOf(max)]
 }
