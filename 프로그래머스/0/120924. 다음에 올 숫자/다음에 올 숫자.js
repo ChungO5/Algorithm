e82eee69689d03c7b957f2d230a1b520899b01ce
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-07 23:38:21 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,6 +1,6 @@
 function solution(common) {
     if (common[0] - common[1] === common[1] - common[2]) {
-        return common.at(-1) * 2 - common.at(-2);
+        return common.at(-1) + (common[1] - common[0])
     } else {
         return common.at(-1) * (common[1] / common[0]);
     }
