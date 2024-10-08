5ecc10373e1ba297b8695aa82c8971a3d1a3c28b
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-08 13:53:57 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    var answer = [];
-    for (let i of numlist){
-        if (i % n === 0){
-            answer.push(i)
-        }
-    }
-    return answer;
+    return numlist.filter((a)=> a%n === 0)
 }
