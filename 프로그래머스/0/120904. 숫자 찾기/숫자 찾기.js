7c288f94caa510fc9412552bd75fcacd02a0da36
BugO <103034239+ChungO5@users.noreply.github.com>
2024-10-15 13:51:51 +0900

    [level 0] Title: 숫자 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,6 +1,5 @@
 function solution(num, k) {
-    let numArr = String(num).split('')
-    let kIndex = numArr.findIndex((el)=> Number(el) === k)
+    let kIndex = [...String(num)].findIndex((el)=> Number(el) === k)
     
     return kIndex < 0 ? -1 : kIndex+1;
 }
